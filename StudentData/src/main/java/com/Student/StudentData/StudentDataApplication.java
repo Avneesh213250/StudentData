0a6d138ad957d662f524cbc1693b1bc0b76f87e3
Avneesh <Avneesh@DESKTOP-F12T56R>
2021-12-21 14:20:05 +0530

    commit

--- StudentData/src/main/java/com/Student/StudentData/StudentDataApplication.java
+++ StudentData/src/main/java/com/Student/StudentData/StudentDataApplication.java
@@ -2,8 +2,10 @@ package com.Student.StudentData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudentDataApplication {
 
 	public static void main(String[] args) {
